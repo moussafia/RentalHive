8fec3e40aa32a02cb9249251627685618ac1f585
moussafia <moussafia.mohammed1999@gmail.com>
2023-11-13 15:22:58 +0100

    first commit

---
+++ src/main/java/ma/youcode/rentalhive/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ma.youcode.rentalhive;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RentalHiveApplication.class);
+    }
+
+}
---
+++ src/test/java/ma/youcode/rentalhive/RentalHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youcode.rentalhive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalHiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
